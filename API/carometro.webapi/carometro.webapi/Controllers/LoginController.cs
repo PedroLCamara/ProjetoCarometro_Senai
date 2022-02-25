7c6d0903350d77d1deba05fbd1e4f80b79a32c2f
Gustavo Miguel Da Silva <44032009821@info.scs>
2022-02-25 17:16:22 -0200

    startup interfaces repositories prontos

---
+++ API/carometro.webapi/carometro.webapi/Controllers/LoginController.cs
@@ -0,0 +1,84 @@
+﻿using carometro.webapi.Domains;
+using carometro.webapi.Interfaces;
+using carometro.webapi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace carometro.webapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+    
+        public LoginController(IUsuarioRepository contexto)
+        {
+            _usuarioRepository = contexto;
+        }
+
+        /// <summary>
+        /// Valida o usuário
+        /// </summary>
+        /// <param name="login">Objeto login que contém o e-mail e a senha do usuário</param>
+        /// <returns>Retorna um token com as informações do usuário</returns>
+        /// dominio/api/Login
+        [HttpPost]
+        public IActionResult Login(LoginViewModel login)
+        {
+            try
+            {
+                Usuario usuarioBuscado = _usuarioRepository.Login(login.Email, login.Senha);
+
+                if (usuarioBuscado == null)
+                {
+                    return Unauthorized(new { msg = "E-mail ou senha invalidos" });
+                }
+
+                // Caso o usuário seja encontrado, prossegue para a criação do token
+
+                /*
+                    Dependências
+
+                    Criar e validar o JWT:      System.IdentityModel.Tokens.Jwt
+                    Integrar a autenticação:    Microsoft.AspNetCore.Authentication.JwtBearer (versão compatível com o .NET do projeto)
+                */
+
+                var minhasClaims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.IdTipoUsuario.ToString())
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("carometro-chave-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var meuToken = new JwtSecurityToken(
+                        issuer: "carometro.webapi",
+                        audience: "carometro.webapi",
+                        claims: minhasClaims,
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(meuToken)
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ API/carometro.webapi/carometro.webapi/Interfaces/IComentarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using carometro.webapi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace carometro.webapi.Interfaces
+{
+    public interface IComentarioRepository
+    {
+        /// <summary>
+        /// Cadastra um novo comentário
+        /// </summary>
+        /// <param name="novoComentario">Comentario a ser cadastrado</param>
+        /// <param name="idAluno">Id do aluno ao qual o comentario pertence</param>
+        void Cadastrar(Comentario novoComentario, int idAluno);
+    }
+}
