985f7fdad27ba32eb478d56a5e0f1598851956df
Pedro Figueira <pedro.figueira2@senaisp.edu.br>
2022-02-25 16:12:27 -0300

    Telas_Com_Funcionamento

--- WEB/carometro_front/src/index.js
+++ WEB/carometro_front/src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import telaLogin from './pages/telaLogin';
+import perfilAluno from './pages/perfilAluno';
+import telaADM from './pages/telaADM';
+
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
+
+const Routing = (
+  <Router>
+    <div>
+      <Routes>
+        <Route exact path="/" component={telaLogin}/>
+        <Route path="/PerfilAluno" component={perfilAluno} />
+        <Route path="/TelaADM" component={telaADM} />
+      </Routes>
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  Routing, document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ WEB/carometro_front/src/pages/telaLogin.jsx
@@ -0,0 +1,11 @@
+
+import { Component } from "react";
+import '../App.css'
+
+export default class Login extends Component{
+    render(){
+        return(
+            <div className="pricipal"></div>
+        );
+    }
+}
