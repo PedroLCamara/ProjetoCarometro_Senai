db79c7166af3585565c1eb84265318851f36bcba
PedroLCamara <plcrlschool@gmail.com>
2022-03-04 13:44:21 -0300

    CorrecoesMerge

--- API/carometro.webapi/carometro.webapi/Contexts/carometroContext.cs
+++ API/carometro.webapi/carometro.webapi/Contexts/carometroContext.cs
@@ -29,11 +29,7 @@ namespace carometro.webapi.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-<<<<<<< HEAD
                 optionsBuilder.UseSqlServer("Data Source=NOTE011EI1\\SQLEXPRESS; initial catalog=carometro; user Id=sa; pwd=Senai@132;");
-=======
-                optionsBuilder.UseSqlServer("Data Source=NOTE0113F3\\SQLEXPRESS; initial catalog=carometro; user Id=sa; pwd=Senai@132;");
->>>>>>> aef5961c603e99451df9daaf8c9acfbcd2ce1631
             }
         }
 
